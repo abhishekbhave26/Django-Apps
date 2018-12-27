ce251761630d3b89ea881a618f7d301d0eee147f
Abhishek Bhave <abhishekbhave26@gmail.com>
2018-12-27 00:59:03 -0500

    Templates & Admin

--- basics_project/news/views.py
+++ basics_project/news/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts=[
+	{
+		'name':'Aaron Ramsey',
+		'title':'Ramsey to leave Arsenal on free',
+		'content':'Arsenal star Aaron Ramsey set to join PSG in a January move for about 10-15 million pounds'
+
+	},
+	{
+		'name':'Chris Smalling',
+		'title':'Chris Smalling signs new contact',
+		'content':'Utd star sign new 150k pounds per week deal under new coach'
+
+	}
+
+]
+
 def home(request):
-    return HttpResponse('<H1> News Page Home </H1>')
     
+    context={'posts':posts}
+    return render(request,'news/home.html',context)
+
+
 def about(request):
-    return HttpResponse('<H1> News About </H1>')
+    return render(request,'news/about.html',{'title':'About'})
     
